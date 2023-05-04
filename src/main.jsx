5aff4fbfc401b0dc907eef99c2fef8997e54b7f8
Jcamilo8aR <camilo8ar23@gmail.com>
2023-05-03 22:36:46 -0500

    Creacion Menu y Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Menu } from './shared/Menu/Menu'
+import { Footer } from './shared/Footer/Footer'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Menu/>
+    <Footer/>
   </React.StrictMode>,
 )
