28ac2b449d6477337c9e84c858ead21a4d9d3332
Jcamilo8aR <camilo8ar23@gmail.com>
2023-05-04 20:30:08 -0500

    Creacion Menu y Footer

---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,209 @@
+export function Footer() {
+    return (
+        <>
+
+
+            <footer className="text-center text-lg-start bg-white text-muted">
+
+
+                <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+
+                    <div className="me-5 d-none d-lg-block">
+                        <span>Conectate con nosotros en nuestras redes sociales:</span>
+                    </div>
+
+
+                    <div>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-google"></i>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </div>
+
+
+                </section>
+
+
+
+
+
+
+                <section className="">
+                    <div className="container text-center text-md-start mt-5">
+
+
+                        <div className="row mt-3">
+
+
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    <i className="text-secondary"></i>Magic Barber
+                                </h6>
+                                <p>
+                                    Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                    dolor sit amet, consectetur adipisicing elit.
+                                </p>
+                            </div>
+
+
+
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+
+                                <h6 className="text-uppercase fw-bold mb-2">
+                                Nuestras redes
+                                </h6>
+                                <div className="mb-1">
+                                <a className="bi bi-instagram text-decoration-none text-secondary"></a>
+                                    <a className="text-decoration-none text-secondary mx-2" href="https://instagram.com">Instagram</a>
+                                </div>
+                                <div className="mb-1">
+                                <a className="bi bi-twitter text-decoration-none text-secondary"></a>
+                                    <a className="text-decoration-none text-secondary mx-2" href="https://twitter.com">Twitter</a>
+                                </div>
+                                <div className="mb-1">
+                                <a className="bi bi-facebook text-decoration-none text-secondary"></a>
+                                    <a className="text-decoration-none text-secondary mx-2" href="https://facebook.com">Facebook</a>
+                                </div>
+                                <div className="mb-1">
+                                <a className="bi bi-github text-decoration-none text-secondary"></a>
+                                    <a className="text-decoration-none text-secondary mx-2" href="https://github.com">GitHub</a>
+                                </div>
+                            </div>
+
+
+                            
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 w-50">
+                                <div className="row">
+                                    <div className="row">
+                                    <h6 className="text-uppercase fw-bold mb-2 text-center">
+                                        Desarrolladores
+                                    </h6>
+                                    </div>
+                                    <div className="col-6 col-md-6">
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Sebastian Giraldo</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Juan Camilo Ochoa</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Alexander Calle</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Santiago Vargas</a>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="col-6 col-md-6">
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Daniel Chica</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Juan Jose Gallego</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Santiago Suarez</a>
+                                        </div>
+                                        <div className="mb-1">
+                                            <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                                <a className="text-decoration-none text-secondary mx-2">Arrieta</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+
+
+
+
+
+                            {/* <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+
+                                <h6 className="text-uppercase fw-bold mb-1">Desarrolladores</h6>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                <div>
+                                    <a href="https://github.com" className="bi bi-github text-secondary"></a>
+                                        <a className="text-decoration-none text-secondary mx-2">Asd</a>
+                                </div>
+                                
+                               
+                            </div> */}
+
+
+                        </div>
+
+
+                    </div>
+                </section>
+
+
+                <div className="text-center p-4">
+                    © 2023 Copyright
+                    {/* <a className="text-reset fw-bold" href="https://mdbootstrap.com/"> MDBootstrap.com</a> */}
+                </div>
+
+
+            </footer>
+
+
+
+
+        </>
+    )
+}
